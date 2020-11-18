2401652a3cdf9c7839af4997f1794fff00edde61
Nathaniel Bleier <nbleier3@illinois.edu>
2020-11-17 20:14:01 -0600

    constant memory

--- custom/gpu-new-forward.h
+++ custom/gpu-new-forward.h
@@ -5,7 +5,10 @@ class GPUInterface
 {
     public:
     void get_device_properties();
-    void conv_forward_gpu(float *host_y, const float *host_x, const float *host_k, const int B, const int M, const int C, const int H, const int W, const int K);
+    void conv_forward_gpu(
+            float* host_y, const float* host_x,
+            const float* host_k, const int B, const int M, const int C,
+            const int H, const int W, const int K);
 };
 
 #endif 
--- custom/new-forward.cu
+++ custom/new-forward.cu
@@ -3,8 +3,27 @@
 #include "gpu-new-forward.h"
 
 #define TILE_WIDTH 16
+#define wbCheck(stmt)                                                         \
+  do {                                                                        \
+    cudaError_t err = stmt;                                                   \
+    if (err != cudaSuccess) {                                                 \
+      fprintf(stderr, "CUDA error: %s", cudaGetErrorString(err));             \
+      fprintf(stderr, "Failed to run stmt %s", #stmt);                        \
+      exit(1);                                                                \
+    }                                                                         \
+  } while (0)
 
-__global__ void conv_forward_kernel(float* y, const float* x, const float* k, const int B, const int M, const int C, const int H, const int W, const int K)
+static constexpr size_t smsize = 1 << 16;
+static constexpr size_t smfloats = smsize / sizeof(float);
+
+__constant__ float c_weights [smfloats];
+
+float* device_k;
+__global__
+void
+conv_forward_kernel(float* y, const float* x, const float* k, const int B,
+                    const int M, const int C, const int H, const int W,
+                    const int K)
 {
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -25,17 +44,20 @@ __global__ void conv_forward_kernel(float* y, const float* x, const float* k, co
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    //(void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    //(void)W_out; // silence declared but never referenced warning. remove this line when you start working
 
-    // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    // An example use of these macros:
-    // float a = y4d(0,0,0,0)
-    // y4d(0,0,0,0) = a
-
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    auto y4d = [&y, M, H_out, W_out](int i3, int i2, int i1, int i0) -> float&
+    {
+        return y[i3*M*H_out*W_out + i2*H_out*W_out + i1*W_out + i0];
+    };
+    auto x4d = [&x, C, H, W](int i3, int i2, int i1, int i0) -> float {
+        return x[i3*C*H*W + i2*H*W + i1*W + i0];
+    };
+    auto k4d = [&k, C, K](int  i3, int i2, int i1, int i0) -> float {
+        return c_weights[i3*C*K*K + i2*K*K + i1*K + i0];
+    };
+// #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+// #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+// #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     // Insert your GPU convolution kernel code here
     const int m = blockIdx.z;
@@ -63,12 +85,15 @@ __global__ void conv_forward_kernel(float* y, const float* x, const float* k, co
 #undef k4d
 }
 
-__host__ void GPUInterface::conv_forward_gpu(float* host_y, const float* host_x, const float* host_k, const int B, const int M, const int C, const int H, const int W, const int K)
+__host__
+void
+GPUInterface::conv_forward_gpu(float* host_y, const float* host_x,
+        const float* host_k, const int B, const int M, const int C,
+        const int H, const int W, const int K)
 {
     // Declare relevant device pointers
     float* device_x;
     float* device_y;
-    float* device_k;
 
     // Allocate memory and copy over the relevant data structures to the GPU
     const unsigned int H_out = H - K + 1;
@@ -77,6 +102,18 @@ __host__ void GPUInterface::conv_forward_gpu(float* host_y, const float* host_x,
     unsigned int inputArrayLength = B*C*H*W;
     unsigned int outputArrayLength = B*M*H_out*W_out;
     unsigned int kernelArrayLength = M*C*K*K;
+
+    // check if kernel weights fit in const memory
+    if (kernelArrayLength > smfloats) {
+        fprintf(stderr,
+                "kernelArrayLength %d does not fit into constant memory\n",
+                kernelArrayLength);
+        exit(1);
+    }
+    else {
+        wbCheck(cudaMemcpyToSymbol(c_weights, host_k, kernelArrayLength *
+                                   sizeof(host_k[0])));
+    }
 
     cudaMalloc(&device_x, inputArrayLength * sizeof(*device_x));
     cudaMalloc(&device_y, outputArrayLength * sizeof(*device_y));
